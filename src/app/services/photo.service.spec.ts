e84cb12adcfd45b406a35db8f29184d8f1648c68
iang79 <iangulo@birt.eus>
2022-10-23 12:50:52 +0200

    Camera and Photo DB

---
+++ src/app/services/photo.service.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+
+import { PhotoService } from './photo.service';
+
+describe('PhotoService', () => {
+  let service: PhotoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PhotoService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+  
+});
+
